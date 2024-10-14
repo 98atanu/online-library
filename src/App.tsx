9da93dbd1d72593ada966e42945edc7fbbd23029
atanu98 <chakrabortyatanu238.com>
2024-10-14 19:16:16 +0530

    shadcn ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
-import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
       <h1>Welcome to Elib- Dashboard </h1>
+      <Button>Click Me</Button>
     </>
   )
 }
